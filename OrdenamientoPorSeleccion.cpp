cd8269741a012c849207000fd04ebd1a32419ce9
Javier Avila Lopez <contacto@javieravilalopez.es>
2025-03-26 20:27:01 +0100

    range-based for loop add in main

--- OrdenamientoPorSeleccion.cpp
+++ OrdenamientoPorSeleccion.cpp
@@ -68,9 +68,9 @@ int main() {
             vector<int> vector(sizeVector);  // Declara el vector de tamaño indicado
 
             // Ingreso de elementos con manejo de errores
-            for (int& i : vector) {
-                cout << "Enter element at position " << i + 1 << ": ";
-                while (!(cin >> vector[i])) {  // Verifica si la entrada es válida
+            for (int& elem : vector) {
+                cout << "Enter element: ";
+                while (!(cin >> elem)) {  // Verifica si la entrada es válida
                     cout << "Invalid input. Please enter an integer: ";
                     cin.clear();  // Limpia el estado de error
                     cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Descartar entrada incorrecta
